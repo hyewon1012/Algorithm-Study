2db3e4bd66d3bb9ca14fb1d3aa34def871c91be7
김혜원 <akrnrh9811@gmail.com>
2021-04-21 00:12:58 +0900

    SWEA_2112_보호필름 풀이 정리

--- Java/SWEA_2112_보호필름_부분집합.java
+++ Java/SWEA_2112_보호필름_부분집합.java
@@ -8,10 +8,10 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class SWEA_2112_보호필름 {
+public class SWEA_2112_보호필름_부분집합 {
 	static int D,W,K;
 	static int[][] map;
-	static int[] statusByRow; // 각 row별 약품 투약 상태 리스트 (-1 : 투여안함,  0: a 투여,  1 : b 투여)
+	static int[][] copymap;
 	static int ans;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,50 +25,63 @@ public class SWEA_2112_보호필름 {
 			K = Integer.parseInt(st.nextToken());
 			
 			map = new int[D][W];
-			statusByRow = new int[D];
+			copymap = new int[D][W];
+			
 			for (int i = 0; i < D; i++) {
 				st = new StringTokenizer(br.readLine(), " ");
 				for (int j = 0; j < W; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
+					copymap[i][j] = map[i][j];
 					
 				}
 			}
 			ans = Integer.MAX_VALUE;
-			dfs(0,0);
+			solve2(0,0);
 			sb.append("#").append(t).append(" ").append(ans).append("\n");
 		}
 		System.out.println(sb);
 	}
 	
-	private static void dfs(int row, int cnt) {
-		//내가 구한 최소값보다 더 크면 더이상 탐색하지 않음
+	
+	//행을 선택하는 부분집합
+	private static void solve2(int row, int cnt) {
 		if(cnt >= ans) return;
-		//맨 마지막에 도달하면 모든 열 연속되있는지 확인하고 답 찾기
 		if(row == D) {
-			if(isContinue()) {
+			if(isContinue2()) {
 				ans = Math.min(ans, cnt);
 			}
 			return;
 		}
+		//1.아무것도 투약 안함
+		solve2(row+1, cnt);
 		
-		for (int i = -1; i < 2; i++) {
-			statusByRow[row] = i;
-			if(i == -1) {
-				dfs(row+1, cnt);
-			}else {
-				dfs(row+1, cnt+1);
-			}
+		//2.A투약
+		for (int i = 0; i < W; i++) {
+			map[row][i] = 0;
 		}
+		solve2(row+1, cnt+1);
+		
+		//3.B투약
+		for (int i = 0; i < W; i++) {
+			map[row][i] = 1;
+		}
+		solve2(row+1, cnt+1);
+		
+		//상태배열 원복
+		for (int i = 0; i < W; i++) {
+			map[row][i] = copymap[row][i];
+		}
+		
 	}
 	
 	
-	private static boolean isContinue() {
+	private static boolean isContinue2() {
 		int now,next;
 		for (int x = 0; x < W; x++) {
 			int cnt = 1; //연속하는원소 카운트
 			for (int y = 0; y < D-1; y++) {
-				now = statusByRow[y] == -1 ? map[y][x] : statusByRow[y];
-				next = statusByRow[y+1] == -1 ? map[y+1][x] : statusByRow[y+1];
+				now = map[y][x];
+				next = map[y+1][x];
 				if(now == next) {
 					cnt++;
 					if(cnt >= K) break;
@@ -85,7 +98,6 @@ public class SWEA_2112_보호필름 {
 		
 		return true;
 	}
-	
 	
 
 }
